10916f32046986511d4b4f88501eacb5e4f24fa5
Spandan Bera <spandanbera1710@gmail.com>
2024-01-06 01:22:28 +0530

    design of login signup form in landing page

--- src/components/LandingForm.jsx
+++ src/components/LandingForm.jsx
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import './LandingForm.css';
+import eye1 from '../pictures/visible.png';
+import { Link, useNavigate } from "react-router-dom";
+import eye2 from '../pictures/hide.png';
 
 const LandingForm = () => {
+    const [id, setId] = useState("");
+    const [pass, setPass] = useState("");
+    const [icon, setIcon] = useState(eye1);
+    const [val, setVal] = useState("password");
+    const navigate = useNavigate();
+
+    const changeId = (e)=> {
+        setId(e.target.value);
+    }
+    const changePass = (e)=> {
+        setPass(e.target.value);
+    }
+    const submitForm = (e)=> {
+        if(id === "" || pass === "")
+        {
+            alert("Please check user id or password.");
+            e.preventDefault();
+        }
+        else
+        {
+            e.preventDefault();
+            setId("");
+            setPass("");
+            navigate("/signup");
+        }
+    }
+    const display = ()=> {
+        if(val === "password")
+        {
+            setIcon(eye2);
+            setVal("text");
+        }
+        else
+        {
+            setIcon(eye1);
+            setVal("password");
+        }
+    }
+    
+
     return(
         <div className="LF-container">
-
+            <form onSubmit={submitForm}>
+                <input type="text" placeholder="user id or email" className="LF-input" value={id} onChange={changeId} />
+                <br />
+                <input type={val} placeholder="password" className="LF-input" value={pass} onChange={changePass} /> 
+                <img src={icon} alt="show hide button" className="LF-show-hide-btn" onClick={display} />
+                <br />
+                <button className="LF-login">Log in</button>
+                <br />
+                <h3 className="LF-h3">Don't have an account? <Link to='/signup' target="blank" className="LF-signup">Sign Up.</Link></h3>
+            </form>
         </div>
     );
 };
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './SignupPage.css';
+
+const SignupPage = ()=> {
+    return(
+        <h1>Sign Up Page</h1>
+    );
+};
+
+export default SignupPage;
