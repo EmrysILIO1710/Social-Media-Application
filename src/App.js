2263286bc87c6cb226c7f47076f617c00ee0d820
Spandan Bera <spandanbera1710@gmail.com>
2024-01-05 12:49:42 +0530

    sign up login form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+// import LandingForm from './components/LandingForm';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <>
-      <h1>Navbar</h1>
+      <LandingPage />
     </>
   );
 }
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './LandingPage.css';
+import LandingForm from "../components/LandingForm";
+
+const LandingPage = () => {
+    return(
+        <>
+            <LandingForm />
+        </>
+    );
+};
+
+export default LandingPage;
