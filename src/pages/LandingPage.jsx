68f53158873a8b9038832ec5d03f82fa11edb2df
Soumi Chakraborty <chakrabortysoumi000@gmail.com>
2024-01-13 13:24:44 +0530

    backdrop created

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import './LandingPage.css';
 import LandingForm from "../components/LandingForm";
 
+
 const LandingPage = () => {
     return(
         <>
             <LandingForm />
+            
+        <div className="LP-content">
+        <h1 className="LP-box-h1">Welcome To <span style={{color: "rgb(254, 163, 82)"}}>adda</span>Gram</h1>
+            <h2>The new way to connect</h2>
+            <h2>Discover new people</h2>
+            <h2>Share memories</h2>
+            
+        </div>
+        
         </>
+
     );
 };
 
