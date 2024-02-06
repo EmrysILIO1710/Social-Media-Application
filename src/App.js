a1554df110a8faec32ac6614599dbae4de9a1b2b
Spandan Bera <spandanbera1710@gmail.com>
2024-02-06 20:50:40 +0530

    customised scrollbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import './components/CustomScrollbar.css';
 // import LandingForm from './components/LandingForm';
 import LandingPage from './pages/LandingPage';
 import SignupPage from './pages/SignupPage';
