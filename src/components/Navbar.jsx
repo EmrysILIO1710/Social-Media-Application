c3f4b50fb3b5a01e8f3af7d808d89fcb4f494043
Sabbir45ali <sabbir84b@gmail.com>
2024-02-24 13:36:13 +0530

    Edited NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,98 +1,189 @@
-import React, { useState } from "react";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+// import { IoIosSearch } from "react-icons/io";
+import { AiFillHome } from "react-icons/ai";
+import AddagramLogo from "../pictures/AddagramLogo.png";
+import { BiSolidMoviePlay } from "react-icons/bi";
+import { BiSolidMessage } from "react-icons/bi";
+import { TbWorldUpload } from "react-icons/tb";
 import "./Navbar.css";
-import Navy from "../pictures/conv.png";
-import Navy2 from '../pictures/message2.png';
-import NS from "../pictures/search.png";
-import NB from "../pictures/notification.png";
-import NB2 from '../pictures/notification2.png';
-import NU from "../pictures/user.png"; 
-import NU2 from '../pictures/user2.png';
 
-const Navbar = () => {
-  const [user, setUser] = useState(NU);
-  const [noti, setNoti] = useState(NB);
-  const [msg, setMsg] = useState(Navy);
-  const [colorUser, setColorUser] = useState("white");
-  const [colorNoti, setColorNoti] = useState("white");
-  const [colorMsg, setColorMsg] = useState("white");
+const navigation = [
+  { name: <AiFillHome size={24} />, href: "#", current: false },
+  { name: <BiSolidMoviePlay size={24} />, href: "#", current: false },
+  { name: <BiSolidMessage size={24} />, href: "#", current: false },
+  { name: <TbWorldUpload size={24} />, href: "#", current: false },
+];
 
-  
-  const changeNoti = () => {
-    if(colorNoti === "white"){
-      setNoti(NB2);
-      setColorNoti("rgb(254, 163, 82)");
-      setMsg(Navy);
-      setColorMsg("white");
-      setUser(NU);
-      setColorUser("white");
-    }
-    else{
-      setNoti(NB);
-      setColorNoti("white");
-    }
-  }
-  const changeMsg = () => {
-    if(colorMsg === "white"){
-      setMsg(Navy2);
-      setColorMsg("rgb(254, 163, 82)");
-      setNoti(NB);
-      setColorNoti("white");
-      setUser(NU);
-      setColorUser("white");
-    }
-    else{
-      setMsg(Navy);
-      setColorMsg("white");
-    }
-  }
-  const changeUser = () => {
-    if(colorUser === "white"){
-      setUser(NU2);
-      setColorUser("rgb(254, 163, 82)");
-      setMsg(Navy);
-      setColorMsg("white");
-      setNoti(NB);
-      setColorNoti("white");
-    }
-    else{
-      setUser(NU);
-      setColorUser("white");
-    }
-  }
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
+export default function Navbar() {
   return (
-    <>  
-      <div className="navbar-rect">
-        <div className="navbar-logo">
-          <h1><span style={{color: "rgb(254,163,82)"}}>adda</span>Gram</h1>
-        </div>
-        <div className="navbar-search">
-          <form >
-            <input className="navbar-searchbar" type="text" name="" id="" placeholder="Search addaGram"/>
-            
-          </form>
-          <img className="navbar-searchicon" src={NS} alt="" />  
-        </div>
-        <div className="navbar-icons">
-          <div style={{backgroundColor: colorNoti}} className="navbar-iconbox">
-            <img className="navbar-noti" src={noti} alt="" onClick={changeNoti} />
+    <Disclosure as="nav" className="shadow-md">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-1 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between ">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden ">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-orange-300 hover:bg-orange-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="h-10 w-auto cursor-pointer"
+                    src={AddagramLogo}
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block my-20">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-orange-400 text-white"
+                            : "text-orange-500 hover:bg-orange-400 hover:text-white",
+                          "rounded-md px-3 no-underline text-sm font-medium hover:transition duration-500"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* Search bar */}
+                <div className="input-group mb-3 my-3 mx-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search"
+                    aria-label="Search"
+                    aria-describedby="button-addon2"
+                  />
+                  {/* <button
+                    className="btn btn-outline-secondary Bg-primary search-icon bg-gray-600 hover:bg-orange-500"
+                    type="button"
+                    id="button-addon2"
+                  >
+                    <IoIosSearch size={20} />
+                  </button> */}
+                </div>
+
+                <button
+                  type="button"
+                  className="relative rounded-full bg-orange-300 p-2 text-black-400 hover:text-white hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 hover:transition duration-500"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-6">
+                  <div>
+                    <Menu.Button className="relative w-24 h-12 flex flex-shrink-0 rounded-full p-2 text-sm focus:outline-none focus:ring-offset-2 focus:ring-offset-gray-800 ">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8 rounded-full"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 text-sm text-gray-700 no-underline"
+                            )}
+                          >
+                            Your Profile
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 text-sm text-gray-700 no-underline"
+                            )}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 text-sm text-gray-700 no-underline"
+                            )}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
           </div>
-          <div style={{backgroundColor: colorMsg}} className="navbar-iconbox">
-            <img className="navbar-msg" src={msg} alt="" onClick={changeMsg} />
-          </div>
-          <div style={{backgroundColor: colorUser}} className="navbar-iconbox">
-            <img className="navbar-user" src={user} alt="" onClick={changeUser} />
-          </div>
-        </div>
-    
-      </div>
-    
-    
-    
-    </>
-    
-      
-    
+
+          <Disclosure.Panel className="sm:hidden shadow-md">
+            <div className="space-y-1 px-2 ">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block rounded-md px-3 text-base font-medium hover:transition duration-500"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 }
-export default Navbar;
