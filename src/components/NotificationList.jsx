2e11b8f741834d8fc02adb12bf6bdfe92a24e50c
Spandan Bera <spandanbera1710@gmail.com>
2024-02-25 20:45:28 +0530

    notification area added to notification button

--- src/components/NotificationList.jsx
+++ src/components/NotificationList.jsx
@@ -4,16 +4,27 @@ import profileimg3 from "../pictures/notificationProfileImg-3.png";
 import { IoVolumeMuteOutline } from "react-icons/io5";
 import { IoIosArrowDown } from "react-icons/io";
 import NotificationItem from "./NotificationItem";
+// import './NotificationList.css';
 
 const NotificationList = () => (
   <div class="relative">
-    <div class="z-10 flex w-screen max-w-max  ">
-      <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 px-4 ring-gray-900/5">
+    <div class="z-10 flex w-screen max-w-max ">
+      <div class="w-screen max-w-md  flex-auto  rounded-3xl bg-white text-sm leading-6 px-4 ring-gray-900/5">
         <div>
           <NotificationItem
             imgSrc={profileimg1}
             title="Yesterday"
             description="krishuu5825, _rohit_rajak_45 and others liked your story. ld"
+          />
+          <NotificationItem
+            imgSrc={profileimg2}
+            title="This Week"
+            description="_rohit_rajak_45 and sayani_0066 liked your story. 2d"
+          />
+          <NotificationItem
+            imgSrc={profileimg3}
+            title="This Month"
+            description="sayani_0066 liked your comment: @joy.singharoy03 @9200payal @_rohit_rajak_45 @sayani_0066 @titly_chakraborty_prathama eee lw"
           />
           <NotificationItem
             imgSrc={profileimg2}
