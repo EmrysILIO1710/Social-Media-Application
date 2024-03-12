a472c4e5428728f021b1c77e7ccc6d9b5a125bfc
Sabbir45ali <sabbir84b@gmail.com>
2024-03-12 22:46:28 +0530

    123

--- src/components/PostOptions.jsx
+++ src/components/PostOptions.jsx
@@ -69,7 +69,7 @@ function PostOptions({ onClose, onUpload }) {
         <div className="flex py-1 max-w-[200px]">
           <img
             className="h-12 w-12 shrink-0 rounded-full object-cover"
-            src="https://images.unsplash.com/photo-1517070208541-6ddc4d3efbcb?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
             alt=""
           ></img>
           <div className="flex items-center mx-3 my-0">
