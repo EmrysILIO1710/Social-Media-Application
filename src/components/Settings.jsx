4974fcce568093bfd1850c6e8cedb48bf7d30a32
Soumi Chakraborty <chakrabortysoumi000@gmail.com>
2024-03-26 08:51:11 +0530

    Settings page created

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,83 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Settings.css";
-import { Button } from "bootstrap";
+import { Button } from "react-bootstrap";
+import { DarkMode } from "@mui/icons-material";
 
 const Settings = () => {
+  const [isDarkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setDarkMode(!isDarkMode);
+  };
+
+  const [selectOption, setSelectOption] = useState("Dark Mode");
+  const optionClick = (option) => {
+    setSelectOption(option);
+  };
+
   return (
     <div className="setting-box">
       <div
-        class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark"
-        style={{ width: "30%", height: "100%" }}
+      id="left-box"
+        className="d-flex flex-column flex-shrink-0 p-3 "
+        style={{ width: "30%", height: "100%", }}
       >
         <a
           href="/"
-          class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none"
+          className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none "
+          
         >
-          <span class="fs-4">Settings & privacy</span>
+          <span className="fs-4">Settings & privacy</span>
         </a>
         <hr />
-        <ul class="nav nav-pills flex-column mb-auto">
-          <li class="nav-item">
-            <a href="#" class="nav-link active" aria-current="page">
+        <ul className="nav nav-pills flex-column mb-auto">
+          <li className="nav-item">
+            <a
+              href="#"
+              className={
+                selectOption === "Dark Mode"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+              onClick={() => optionClick("Dark Mode")}
+            >
               Dark Mode
             </a>
           </li>
-          <li>
-            <a href="#" class="nav-link text-white">
+
+          <li className="nav-item">
+            <a
+              href="#"
+              className={
+                selectOption === "Settings & privacy"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+              onClick={() => optionClick("Settings & privacy")}
+            >
               Settings & privacy
             </a>
           </li>
         </ul>
         <hr />
-        <div class="dropdown">
-          <a
-            href="#"
-            class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <img
-              src="https://github.com/mdo.png"
-              alt=""
-              width="32"
-              height="32"
-              class="rounded-circle me-2"
-            />
-            <strong>mdo</strong>
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-            <li>
-              <a class="dropdown-item" href="#">
-                New project...
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                Profile
-              </a>
-            </li>
-            <li>
-              <hr class="dropdown-divider" />
-            </li>
-            <li>
-              <a class="dropdown-item" href="#">
-                Sign out
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
 
-      <div className="setting-box2">
-      <h3 style={{color: "black", fontFamily:"opensans-bold", marginTop:"20%",}}> Switch To Dark Mode </h3>
-        <label class="switch">
-            <span class="slider"></span>
-        </label>
+      <div className={isDarkMode ? "setting-box2 dark-mode" : "setting-box2"}>
+        {selectOption === "Dark Mode" ? (
+          <div>
+          <h3
+            style={{
+              color: isDarkMode ? "white" : "black",
+              fontFamily: "opensans-bold",
+              marginTop: "20%",
+            }}
+          >
+            {isDarkMode ? "switch to light mode" : "switch to dark mode"}
+          </h3>
+          <label className={`switch ${isDarkMode ? "clicked" : ""}`} onClick={toggleDarkMode}>
+          <span className="slider"></span>
+        </label> 
+        </div>
+        ) : (
+          <div>
+            <h3>Change Password</h3>
+            <input
+              type="password"
+              placeholder="Enter Current Password"
+              
+            />
+            <input
+              type="password"
+              placeholder="Enter New Password"
+              
+            />
+            <input
+            type="password"
+            placeholder="Confirm Password"
+            />
+
+            <Button> Change Password</Button>
+          </div>
+        )}
+
+        
       </div>
     </div>
   );
