8abe35719b88834026d68f7f172b05174838c890
Spandan Bera <spandanbera1710@gmail.com>
2024-03-28 09:40:32 +0530

    logout funtionality added

--- src/pages/Mutuals.jsx
+++ src/pages/Mutuals.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import SideBar from "../components/SideBar";
 import SimpleContainer from "../components/SimpleContainer";
+import { useLocation } from "react-router-dom";
 
 const Mutuals = (props) => {
   const [modebg, setModebg] = useState("rgb(255, 246, 234)");
-
+  const location = useLocation();
   useEffect(() => {
     if (props.mode) {
       //dark mode
@@ -16,7 +17,10 @@ const Mutuals = (props) => {
       setModebg("rgb(255, 246, 234)");
     }
     props.handleCB(true);
-  }, [props]);
+    if (!location.hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [location, props]);
   return (
     <>
       <div style={{ backgroundColor: modebg, width: "100%", height: "auto"}}>
