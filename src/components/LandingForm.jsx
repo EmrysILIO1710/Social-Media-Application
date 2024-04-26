2907c8ca6044dfbd747316e5ba2532acf87125b1
Sabbir45ali <sabbir84b@gmail.com>
2024-04-26 21:18:53 +0530

    Landing page Signup responsive

--- src/components/LandingForm.jsx
+++ src/components/LandingForm.jsx
@@ -1,87 +1,98 @@
 import React, { useState } from "react";
-import './LandingForm.css';
-import eye1 from '../pictures/visible.png';
+import "./LandingForm.css";
+import eye1 from "../pictures/visible.png";
 import { useNavigate } from "react-router-dom";
-import eye2 from '../pictures/hide.png';
+import eye2 from "../pictures/hide.png";
 import SignupPage from "../pages/SignupPage";
 // import { GoogleLogin } from '@react-oauth/google';
 // import { jwtDecode } from "jwt-decode";
-import { useGoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
+import SignUpPageResponsive from "./SignUpPageResponsive";
 
 const LandingForm = () => {
-    const [id, setId] = useState("");
-    const [pass, setPass] = useState("");
-    const [icon, setIcon] = useState(eye1);
-    const [val, setVal] = useState("password");
-    const [show, setShow] = useState(false);
-    
-    const navigate = useNavigate();
+  const [id, setId] = useState("");
+  const [pass, setPass] = useState("");
+  const [icon, setIcon] = useState(eye1);
+  const [val, setVal] = useState("password");
+  const [show, setShow] = useState(false);
 
-    const changeId = (e)=> {
-        setId(e.target.value);
-    }
-    const changePass = (e)=> {
-        setPass(e.target.value);
-    }
-    const submitForm = (e)=> {
-        if(id === "" || pass === "")
-        {
-            alert("Please check user id or password.");
-            e.preventDefault();
-        }
-        else
-        {
-            e.preventDefault();
-            setId("");
-            setPass("");
-            navigate("/feed");
-        }
-    }
-    const display = ()=> {
-        if(val === "password")
-        {
-            setIcon(eye2);
-            setVal("text");
-        }
-        else
-        {
-            setIcon(eye1);
-            setVal("password");
-        }
-    }
-    const login = useGoogleLogin({
-        onSuccess: async (response) => {
-            try{
-                const res = await axios.get(
-                    "https://www.googleapis.com/oauth2/v3/userinfo",
-                    {
-                        headers: {
-                            Authorization: `Bearer ${response.access_token}`,
-                        },
-                    }
-                );
-                console.log(res);
-                navigate("/feed");
-            }
-            catch(err){
-                console.log(err);
-            }
-        },
-    });
+  const navigate = useNavigate();
 
-    return(
-        <>
-        <div className="LF-container">
-            <form onSubmit={submitForm}>
-                <input type="text" placeholder="user id or email" className="LF-input" value={id} onChange={changeId} />
-                <br />
-                <input type={val} placeholder="password" className="LF-input" value={pass} onChange={changePass} /> 
-                <img src={icon} alt="show hide button" className="LF-show-hide-btn" onClick={display} />
-                <br />
-                <button className="LF-login">Log in</button>
-                {/* <br /> */}
-                {/* <GoogleLogin
+  const changeId = (e) => {
+    setId(e.target.value);
+  };
+  const changePass = (e) => {
+    setPass(e.target.value);
+  };
+  const submitForm = (e) => {
+    if (id === "" || pass === "") {
+      alert("Please check user id or password.");
+      e.preventDefault();
+    } else {
+      e.preventDefault();
+      setId("");
+      setPass("");
+      navigate("/feed");
+    }
+  };
+  const display = () => {
+    if (val === "password") {
+      setIcon(eye2);
+      setVal("text");
+    } else {
+      setIcon(eye1);
+      setVal("password");
+    }
+  };
+  const login = useGoogleLogin({
+    onSuccess: async (response) => {
+      try {
+        const res = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${response.access_token}`,
+            },
+          }
+        );
+        console.log(res);
+        navigate("/feed");
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
+
+  return (
+    <>
+      <div className="LF-container">
+        <form onSubmit={submitForm}>
+          <input
+            type="text"
+            placeholder="user id or email"
+            className="LF-input"
+            value={id}
+            onChange={changeId}
+          />
+          <br />
+          <input
+            type={val}
+            placeholder="password"
+            className="LF-input"
+            value={pass}
+            onChange={changePass}
+          />
+          <img
+            src={icon}
+            alt="show hide button"
+            className="LF-show-hide-btn"
+            onClick={display}
+          />
+          <br />
+          <button className="LF-login">Log in</button>
+          {/* <br /> */}
+          {/* <GoogleLogin
                     onSuccess={credentialResponse => {
                         var credentialResponseDecoded = jwtDecode(credentialResponse.credential);
                         console.log(credentialResponseDecoded);
@@ -90,14 +101,29 @@ const LandingForm = () => {
                         console.log('Login Failed');
                     }}
                 /> */}
-                <button style={{margin: "0px 112px", fontSize: "18px"}} onClick={() => login()}>Continue with Google</button>
-                <br />
-                <h3 className="LF-h3">Don't have an account? <span className="LF-signup" onClick={()=> setShow(true)}>Sign Up.</span></h3>
-            </form> 
-        </div>
-        <SignupPage onClose={()=> setShow(false)} show={show} />
-        </>
-    );
+          <button
+            style={{ margin: "0px 112px", fontSize: "18px" }}
+            onClick={() => login()}
+          >
+            Continue with Google
+          </button>
+          <br />
+          <h3 className="LF-h3">
+            Don't have an account?{" "}
+            <span className="LF-signup" onClick={() => setShow(true)}>
+              Sign Up.
+            </span>
+          </h3>
+        </form>
+      </div>
+      <div className="sm:block md:block lg:hidden xl:hidden">
+        <SignUpPageResponsive onClose={() => setShow(false)} show={show} />
+      </div>
+      <div className="hidden sm:block">
+        <SignupPage onClose={() => setShow(false)} show={show} />
+      </div>
+    </>
+  );
 };
 
 export default LandingForm;
