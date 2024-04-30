3bf766191dcfcf19049ca57dd06a151bcafd9a81
Spandan Bera <spandanbera1710@gmail.com>
2024-05-01 00:34:06 +0530

    fixing landing page form

--- src/App.js
+++ src/App.js
@@ -139,7 +139,10 @@ function App() {
           <Route path='/search' element={<BottomSearch handleCB = {CB} mode={modeTrigger} />} />
         </Routes>
         {/* <PostButton/> */}
-        <LabelBottomNavigation mode={modeTrigger} />
+        {
+          show && <LabelBottomNavigation mode={modeTrigger} />
+        }
+        
       </BrowserRouter>
     </>
   );
