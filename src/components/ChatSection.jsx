bf688229233642336bd5cb0bf0aec6c41e8f725e
Sabbir45ali <sabbir84b@gmail.com>
2024-05-02 21:37:45 +0530

    Commit for message section function workng

--- src/components/ChatSection.jsx
+++ src/components/ChatSection.jsx
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 import { IoIosSend } from "react-icons/io";
+import EmojiPicker from "emoji-picker-react";
+import smiley from "../pictures/smiley.png";
 
 const ChatSection = () => {
   const [messages, setMessages] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [picker, setPicker] = useState(false);
+
+  const openPicker = () => {
+    setPicker(!picker);
+  };
+
+  const addEmoji = (e) => {
+    setInputValue(inputValue + e.emoji);
+  };
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -15,12 +26,10 @@ const ChatSection = () => {
     setMessages((prevMessages) => [inputValue, ...prevMessages]);
     setInputValue("");
   };
-//chat section
+  //chat section
   return (
     <>
-      <div className="w-full bg-white shadow-md top-0 mt-16 h-12">
-
-      </div>
+      <div className="w-full bg-white shadow-md top-0 mt-16 h-12"></div>
       <div className="lg:hidden fixed bottom-0 mb-16 w-full bg-white p-2">
         <div className="flex flex-col-reverse h-full overflow-y-auto">
           {messages.map((message, index) => (
@@ -31,21 +40,44 @@ const ChatSection = () => {
             </div>
           ))}
         </div>
-        <form onSubmit={handleSubmit} className="mt-2 flex">
-          <input
-            type="text"
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="Type your message..."
-            className="w-full rounded-full border border-gray-300 px-4 py-2 focus:outline-none"
-          />
-          <button
-            type="submit"
-            className="bg-orange-400 text-white rounded-full px-4 py-2 ml-2"
-          >
-            <IoIosSend />
-          </button>
-        </form>
+        <div className="px-2 sm:px-6">
+          <form onSubmit={handleSubmit}>
+            <label className="relative block mt-4 flex">
+              <span className="absolute bottom-[120%] right-0">
+                <EmojiPicker
+                  height={380}
+                  width={300}
+                  open={picker}
+                  onEmojiClick={addEmoji}
+                />
+              </span>
+              <input
+                className="placeholder:italic placeholder:text-gray-500 text-wrap block bg-white w-full rounded-full border border-slate-300 py-2 px-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                placeholder="write a message..."
+                type="text"
+                name="search"
+                value={inputValue}
+                onChange={handleInputChange}
+              />
+              <span className="mx-2 my-2 cursor-pointer">
+                <img
+                  src={smiley}
+                  alt=""
+                  onClick={openPicker}
+                  style={{ width: "50px" }}
+                />
+              </span>
+              <span className="mx-2 mt-2 cursor-pointer">
+                <button
+                  type="submit"
+                  className="bg-orange-400 text-white rounded-full px-3 py-2 ml-2"
+                >
+                  <IoIosSend size={20} />
+                </button>
+              </span>
+            </label>
+          </form>
+        </div>
       </div>
     </>
   );
